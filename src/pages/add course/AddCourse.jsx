be861ad7c51cab7138ad391fd8afd4d23a4c13f7
moh0x <141264101+moh0x@users.noreply.github.com>
2025-05-23 11:10:44 +0000

    Update AddCourse.jsx

--- src/pages/add course/AddCourse.jsx
+++ src/pages/add course/AddCourse.jsx
@@ -18,10 +18,11 @@ function AddCourse() {
             "Depart":"",
             "assurance":"",
             "kilomitrage":"",
-            "marque":""
+            "marque":"",
+            "priceParKilomitre":""
         }) 
         async function click(e) {
-            if (formData.matricule.length < 1 || formData.matricule.length < 10 || formData.Depart.length < 3 || formData.Depart.length > 50 || formData.Destination.length < 3 || formData.Destination.length > 50 || formData.marque.length < 3 || formData.marque.length > 50 ) {
+            if (formData.matricule.length < 1 || formData.matricule.length < 10 || formData.Depart.length < 3 || formData.Depart.length > 50 || formData.Destination.length < 3 || formData.Destination.length > 50 || formData.marque.length < 3 || formData.marque.length > 50 || formData.priceParKilomitre.length < 1|| formData.priceParKilomitre.length > 50 ) {
    
               e.preventDefault()
             } else {
@@ -61,6 +62,7 @@ function AddCourse() {
         <input type="text" placeholder="assurance" className="form-control bg-dark text-white mb-3" aria-label="Small" aria-describedby="inputGroup-sizing-sm" name="assurance"  onChange={(e)=>formHandler(e)}/>
         <input type="text" placeholder="kilomitrage" className="form-control bg-dark text-white mb-3" aria-label="Small" aria-describedby="inputGroup-sizing-sm" name="kilomitrage"  onChange={(e)=>formHandler(e)} minLength={3} maxLength={50}/>
         <input type="text" placeholder="marque" className="form-control bg-dark text-white mb-3" aria-label="Small" aria-describedby="inputGroup-sizing-sm" name="marque"  onChange={(e)=>formHandler(e)}/>
+        <input type="text" placeholder="price par kilomitre" className="form-control bg-dark text-white mb-3" aria-label="Small" aria-describedby="inputGroup-sizing-sm" name="priceParKilomitre"  onChange={(e)=>formHandler(e)}/>
         {load ? <div className='text-center'><FourSquare color="#3bc5f3" size="medium" text="" textColor="" /></div>: <h2 className="text-white col btn btn-primary fw-bold p-2" onClick={(e)=>click(e)}>Add Trip <IoAddCircleSharp /></h2>}
         <ErrAuth clicked={isClicked} err={err} type=""/>
         </div>
